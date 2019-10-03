e626d18e0fa10a9345be7feb3a5330a43b699b22
Vitor M. A. da Cruz <vitor.mazzi@intelie.com.br>
2019-10-03 19:14:58 -0300

    The chatbot features now have access to an event sender function

--- events/raw.py
+++ events/raw.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from live_client.utils.timestamp import get_timestamp
+from live_client.utils import logging
 
 __all__ = [
     'format_event',
@@ -7,14 +8,20 @@ __all__ = [
 ]
 
 
-def format_and_send(event_type, statuses, settings, connection_func=None):
-    timestamp = statuses.pop('timestamp', get_timestamp())
-    event = format_event(timestamp, event_type, statuses, settings)
+def create(event_type, event_data, process_settings=None, output_info=None):
+    connection_func, output_settings = output_info
+    logging.debug(f'Creating raw event of type "{event_type}": {event_data}')
+    format_and_send(event_type, event_data, output_settings, connection_func=connection_func)
+
+
+def format_and_send(event_type, event_data, settings, connection_func=None):
+    timestamp = event_data.pop('timestamp', get_timestamp())
+    event = format_event(timestamp, event_type, event_data, settings)
     connection_func(event, settings)
 
 
-def format_event(timestamp, event_type, statuses, settings):
-    event_data = statuses.copy()
+def format_event(timestamp, event_type, event_data, settings):
+    event_data = event_data.copy()
     event_data['__type'] = event_type
     event_data['liverig__index__timestamp'] = timestamp
     return event_data
