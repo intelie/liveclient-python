fed23430c194d31bca9b1f75d35537b522829a54
Eric Chiesse <eric.chiesse@intelie.com.br>
2020-03-06 16:30:48 -0300

    Changed the parameters order in the test to the desired one so that it will be the same as the funcion in raw.py. Tests are now failing.

--- test/test_events_messenger.py
+++ test/test_events_messenger.py
@@ -14,15 +14,15 @@ class TestFormatEvent:
 
     def test_base_event_configured(self):
         required_keys = ["uid", "__type", "createdAt"]
-        event = messenger.format_event("__type__", 0, {})
+        event = messenger.format_event({}, 0, "__type__")
         assert dict_has_valid_items(event, required_keys)
 
     def test_formatted_event_has_event_type(self):
         event_type = "__event_type__"
-        event = messenger.format_event(event_type, "_", {})
+        event = messenger.format_event({}, 0, event_type)
         assert event.get("__type") == event_type
 
     def test_formatted_event_has_timestamp(self):
         timestamp = now_timestamp()
-        event = messenger.format_event("_", timestamp, {})
+        event = messenger.format_event({}, timestamp, "_")
         assert event.get("createdAt") == timestamp
