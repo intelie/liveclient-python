dcab3dbfbe53648590998f363031db6503563ffb
Eric Chiesse <eric.chiesse@intelie.com.br>
2020-03-06 16:11:23 -0300

    First tests to events/messenger.py

---
+++ test/predicates.py
@@ -0,0 +1,10 @@
+def dict_has_valid_items(d, keys):
+    """ Tests if the items exist and are different from None """
+    return all(map(lambda key: d.get(key) is not None, keys))
+
+
+def dict_contains(d, other):
+    for k, v in other.items():
+        if d.get(k) != v:
+            return False
+    return True
---
+++ test/test_events_messenger.py
@@ -0,0 +1,28 @@
+from unittest import mock
+
+from testbase import *
+
+from live_client.events import messenger
+from predicates import *
+
+
+class TestFormatEvent:
+    def test_formatted_event_is_not_the_original(self):
+        event = {}
+        formatted_event = messenger.format_event("__type__", 0, event)
+        assert event is not formatted_event
+
+    def test_base_event_configured(self):
+        required_keys = ["uid", "__type", "createdAt"]
+        event = messenger.format_event("__type__", 0, {})
+        assert dict_has_valid_items(event, required_keys)
+
+    def test_formatted_event_has_event_type(self):
+        event_type = "__event_type__"
+        event = messenger.format_event(event_type, "_", {})
+        assert event.get("__type") == event_type
+
+    def test_formatted_event_has_timestamp(self):
+        timestamp = now_timestamp()
+        event = messenger.format_event("_", timestamp, {})
+        assert event.get("createdAt") == timestamp
