e80ec0791cb75b47ffdeb51595cc7f97914b52eb
Eric Chiesse <eric.chiesse@intelie.com.br>
2020-03-13 14:35:52 -0300

    Fixed buffer to be an instance variable instead of a class variable. Added method is_empty.

--- test/testbase.py
+++ test/testbase.py
@@ -23,10 +23,14 @@ def raises(exception_cls, f, *args, **kwargs):
 
 
 class Collector:
-    buffer = []
+    def __init__(self):
+        self.buffer = []
 
     def collect(self, obj):
         self.buffer.append(obj)
 
     def __call__(self, obj):
         self.collect(obj)
+
+    def is_empty(self):
+        return len(self.buffer) == 0
