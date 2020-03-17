87a92d30b003e638f5ac473286cdcfe68c096de2
Eric Chiesse <eric.chiesse@intelie.com.br>
2020-03-17 19:35:18 -0300

    Created class to simulate chat room insertion and removal of users.

---
+++ test/mocks.py
@@ -0,0 +1,24 @@
+from unittest import mock
+
+
+class ChatMock:
+    def __init__(self):
+        self.room = {
+            "users": {},
+        }
+
+    def update_room(self, event):
+        for user in event.get("removedUsers", []):
+            removed_user = self.room["users"].pop(user["id"])
+
+        for user in event.get("addedOrUpdatedUsers", []):
+            self.room["users"][user["id"]] = user
+
+
+# [ECS]: "patch_with_factory" is not the best name for this function
+# The intention here is to provide a simple way to return a function that when
+# called will return the desired object. I'm calling such function a "factory"
+# but it may be misleading for people comming from other contexts.
+# While we don't come up with a better name, let's use this.
+def patch_with_factory(target, obj, *args, **kwargs):
+    return mock.patch(target, lambda *a, **kws: obj, *args, **kwargs)
