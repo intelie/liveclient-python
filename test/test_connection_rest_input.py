ea0ab75aa53423211a7b464714543df6547d1a4f
Eric Chiesse <eric.chiesse@intelie.com.br>
2020-04-08 18:21:27 -0300

    Added tests for connection.rest_input.send_event.

---
+++ test/test_connection_rest_input.py
@@ -0,0 +1,53 @@
+from unittest import mock
+import requests
+
+from testbase import *
+
+from live_client.connection import rest_input
+from predicates import *
+from mocks import *
+from utils import settings as S
+
+DEFAULT_SETTINGS = {param: "" for param in rest_input.REQUIRED_PARAMETERS}
+
+class TestSendEvent:
+    def test_returns_false_if_no_or_empty_event(self):
+        assert rest_input.send_event(None, {}) == False
+        assert rest_input.send_event({}, {}) == False
+
+    def test_raises_if_no_settings(self):
+        assert raises(Exception, rest_input.send_event, {"message": "_"}, None)
+
+    def test_raises_on_invalid_settings(self):
+        assert raises(
+            Exception,
+            rest_input.send_event,
+            {"message": "_"},
+            {"url": "","username": "", "password": ""}
+        )
+        assert raises(
+            Exception,
+            rest_input.send_event,
+            {"message": "_"},
+            {"url": "","username": "", "rest_input": ""}
+        )
+        assert raises(
+            Exception,
+            rest_input.send_event,
+            {"message": "_"},
+            {"url": "","password": "", "rest_input": ""}
+        )
+        assert raises(
+            Exception,
+            rest_input.send_event,
+            {"message": "_"},
+            {"username": "", "password": "", "rest_input": ""}
+        )
+
+    @mock.patch("requests.Session.post")
+    @mock.patch("requests.Response.raise_for_status")
+    def test_attempts_to_send_event_if_valid_data(self, raise_for_status_mock, post_mock):
+        res = rest_input.send_event({"message": "_"}, DEFAULT_SETTINGS)
+        print(post_mock.call_args)
+        print(raise_for_status_mock.call_args)
+        assert res == True
