b8443d1c46c58e8ba1042b2a0af167c10c146ac7
Eric Chiesse <eric.chiesse@intelie.com.br>
2020-04-16 18:21:28 -0300

    Created tests for "tcp_input.send_event".

---
+++ test/test_connection_tcp_input.py
@@ -0,0 +1,55 @@
+from testbase import *
+
+import json
+import multiprocessing as mp
+import queue
+import requests
+import socket
+import time
+
+from unittest import mock
+
+from live_client.connection import tcp_input
+from mocks import *
+from predicates import *
+from utils import settings as S
+from utils.micro_tcp import MicroTcpServer
+
+#DEFAULT_SETTINGS = {param: "" for param in tcp_input.REQUIRED_PARAMETERS}
+SERVER_ADDR = ("127.0.0.1", 8889)
+DEFAULT_SETTINGS = {
+    "ip": SERVER_ADDR[0],
+    "port": SERVER_ADDR[1],
+}
+
+
+class TestSendEvent:
+    def test_event_is_sent(self):
+        server = MicroTcpServer(SERVER_ADDR)
+        time.sleep(.1)
+
+        message = "!!Testando !!"
+        tcp_input.send_event({"message": message}, DEFAULT_SETTINGS)
+
+        try:
+            message_back = json.loads(server.output_queue.get())
+            server.close()
+        except queue.Empty as e:
+            message_back = None
+
+        assert message_back["message"] == message
+
+    def test_no_event_sent_if_no_message(self):
+        server = MicroTcpServer(SERVER_ADDR)
+        time.sleep(.1)
+
+        tcp_input.send_event({}, DEFAULT_SETTINGS)
+        try:
+            message = server.output_queue.get(True, 1)
+            assert False # If we are here the message has been sent
+        except queue.Empty as e:
+            server.close()
+
+    def test_raises_on_invalid_settings(self):
+        assert raises(KeyError, tcp_input.send_event, {"message": "_"}, {"ip": "127.0.0.1"})
+        assert raises(KeyError, tcp_input.send_event, {"message": "_"}, {"port": 3210})
---
+++ test/utils/micro_tcp.py
@@ -0,0 +1,36 @@
+import multiprocessing as mp
+import socket
+
+
+class MicroTcpServer():
+    def __init__(self, server_addr):
+        self.output_queue = mp.Queue()
+        self.process = mp.Process(target = self.run, args=[server_addr, self.output_queue])
+        self.process.start()
+
+    def run(self, server_addr, output_queue):
+        # Create the socket and listen:
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while True:
+            try:
+                serversocket.bind(server_addr)
+                serversocket.listen(5)
+                break
+            except OSError:
+                raise
+
+        # Wait for a connection and message:
+        (clientsocket, address) = serversocket.accept()
+        buffer, sender = clientsocket.recvfrom(1024)
+        message = buffer.decode("utf-8")
+        output_queue.put(message)
+        serversocket.shutdown(socket.SHUT_RDWR)
+        serversocket.close()
+
+    def close(self):
+        if self.process.is_alive():
+            while not self.output_queue.empty():
+                self.output_queue.get(False)
+            self.output_queue.close()
+            self.output_queue.join_thread()
+            self.process.terminate()
