70e71f9b28e90d7a2e796c5dc3b9f84c6f0f09cc
Eric Chiesse <eric.chiesse@intelie.com.br>
2020-04-27 19:22:04 -0300

    Created tests for "utils.timestamp".

---
+++ test/test_utils_timestamp.py
@@ -0,0 +1,20 @@
+from testbase import *
+
+import time
+from unittest import mock
+
+from live_client.utils import timestamp as TS
+from mocks import *
+from predicates import *
+
+
+class TestGetTimestamp:
+    def test_timestamp_is_integer(self):
+        ts = TS.get_timestamp()
+        assert type(ts) is int
+
+    def test_last_timestamp_is_higher_than_previous(self):
+        ts1 = TS.get_timestamp()
+        time.sleep(.01)
+        ts2 = TS.get_timestamp()
+        assert ts2 > ts1
