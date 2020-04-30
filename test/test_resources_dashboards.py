e480f27d1fc5471476362262d2721d30c4455b01
Eric Chiesse <eric.chiesse@intelie.com.br>
2020-04-30 20:04:59 -0300

    Created tests for "resources.dashbords"

---
+++ test/test_resources_dashboards.py
@@ -0,0 +1,32 @@
+from testbase import *
+
+import copy
+import pytest
+import requests
+
+from unittest import mock
+
+from live_client.resources import dashboards
+from mocks import *
+from predicates import *
+from utils import vcrutils
+
+DEFAULT_SETTINGS = {
+    "live": {
+        "username": "admin",
+        "password": "admin",
+        "url": "http://localhost:8080",
+    }
+}
+
+
+def _build_default_settings():
+    return copy.deepcopy(DEFAULT_SETTINGS)
+
+
+class TestListDashboards:
+    @mock.patch("live_client.resources.base.fetch_resource")
+    def test_calls_the_correct_endpoint(self, mock_fetch_resource):
+        settings = _build_default_settings()
+        dashboards.list_dashboards(settings)
+        assert mock_fetch_resource.called_with("/rest/dashboard", settings)
